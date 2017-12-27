4c6554a59f00104ad144aa0cd1aba25d54240869
Matthew <mtso@tuta.io>
2017-12-27 09:07:19 -0800

    Add defaultLink to BackButton

--- BackButton.jsx
+++ BackButton.jsx
@@ -4,10 +4,12 @@ import { Link, withRouter } from 'react-router-dom'
 class BackButton extends Component {
   goBack = (e) => {
     e.preventDefault()
-    
-    const { history } = this.props
+    const { history, defaultLink } = this.props
+
     if (history.length > 1) {
       history.goBack()
+    } else {
+      history.push(defaultLink)
     }
   }
 
